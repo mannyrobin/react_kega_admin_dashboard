5a210f005acfc2fdc8d40bcf97ebc37417c415e0
mannyrobin <69haiqian@gmail.com>
2018-05-29 22:03:14 +0400

    Suro

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -17,6 +17,14 @@ class HeaderLinks extends Component{
                     <NavItem eventKey={1} href="#" className="icon-notification circle-number"><span>{this.props.props.data.notification_count && this.props.props.data.notification_count}</span></NavItem>
                     <NavItem eventKey={1} href="#" className="icon-messages circle-number"><span>{this.props.props.data.message_count && this.props.props.data.message_count}</span></NavItem>
                     <NavItem eventKey={3} href="#" className="user-icon"><img id="userIcon" src={this.props.props.data.icon_url && this.props.props.data.icon_url}/><span>{this.props.props.data.name && this.props.props.data.name}</span></NavItem>
+                    <div className="custom-drop-down">
+                        <div className="custom-drop-down-inner">
+                            <span>Notification</span>
+                            <span>Notification</span>
+                            <span>Notification</span>
+                            <span>Notification</span>
+                        </div>
+                    </div>
                 </Nav>
             </div>
         );
